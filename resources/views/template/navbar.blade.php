5a55c07c84431b880b5d4c2ce95f805c91f89ce8
Bulzart <86388058+bulzart@users.noreply.github.com>
2021-12-15 14:38:34 +0100

    documentform

--- resources/views/template/navbar.blade.php
+++ resources/views/template/navbar.blade.php
@@ -275,7 +275,6 @@ a:link{
                     <a style="text-decoration: none;" href="{{route('dashboard')}}" class="nav_link active"> <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Home</span> </a>
                     @if(Auth::guard('admins')->check())<a style="text-decoration: none;" href="" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Costumers</span> </a>@endif
                     @if(Auth::guard('admins')->user()->role == 'backoffice' || Auth::guard('admins')->user()->role == 'fieldservice' || Auth::guard('admins')->user()->role == 'admin')<a href="#" style="text-decoration: none;" class="nav_link"> <i class='bx bx-task' ></i> <span class="nav_name">Tasks</span> </a>@endif
-=======
             <div> 
                 <div class="nav_list"> 
                     <a style="text-decoration: none;" href="{{route('dashboard')}}" class="nav_link active"> <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Home</span> </a> 
