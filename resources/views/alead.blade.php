236bc841a8d9e91a332e859a5a15fd974a457a04
Bulzart <86388058+bulzart@users.noreply.github.com>
2021-12-16 12:37:02 +0100

    123

--- resources/views/alead.blade.php
+++ resources/views/alead.blade.php
@@ -33,9 +33,9 @@
   </select>
 
 
-  <input type="submit" class="btn btn-primary mt-2" value="Assign">
+  <input type="submit" class="btn btn-primary mt-2" value="Appointment">
 </form>
-</div>
+
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
