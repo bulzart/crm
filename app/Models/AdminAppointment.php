4fc11bf8776b62109ead98669e5fc8f26d5eeb40
Bulzart <86388058+bulzart@users.noreply.github.com>
2021-12-27 17:30:56 +0100

    123

---
+++ app/Models/AdminAppointment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AdminAppointment extends Model
+{
+    use HasFactory;
+}
