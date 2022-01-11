57f00111f947f31a6df7079d7dc860e3ee2c3bbd
Albenit <35097523+Albenit@users.noreply.github.com>
2022-01-11 17:03:52 +0100

    a

--- app/Imports/LeadImport.php
+++ app/Imports/LeadImport.php
@@ -2,53 +2,62 @@
 
 namespace App\Imports;
 
+use App\Models\Admins;
+use App\Models\campaigns;
 use App\Models\lead;
-use Dotenv\Validator;
-use Illuminate\Support\Collection;
-use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
 
-class LeadImport implements ToModel,ToCollection, WithHeadingRow
+class LeadImport implements ToModel
 {
     /**
     * @param array $row
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
-    public function model(array $row)
-    {
-        return new lead([
+    public $latitude,$longitude;
+    public $admin,$camp;
 
-        ]);
+    public function latitudee($address1){
+        $address = $address1;
+        $url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' . urlencode($address) . '&key=AIzaSyDscxZzYju_pJGNA2zu1lXOqJuubCdPu0o';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $responseJson = curl_exec($ch);
+        curl_close($ch);
+        $response = json_decode($responseJson);
+        if ($response->status == 'OK') {
+            $latitude = $response->results[0]->geometry->location->lat;
+            $longitude = $response->results[0]->geometry->location->lng;
+        }
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
     }
 
-    public function collection(Collection $rows)
+    public function model(array $row)
     {
-//        Validator::make($rows->toArray(),[
-//            '*.fname' => 'required',
-//            '*.lname' => 'required',
-//            '*.phone' => 'required',
-//            '*.address' => 'required',
-//            '*.postal' => 'required',
-//            '*.location' => 'required',
-//            '*.count' => 'min:1',
-//            '*.apptime' => 'required',
-//            '*.appbirthdate' => 'required'
-//        ])->validate();
-        foreach ($rows as $row){
-            lead::create([
-                'fname' => $row['first_name'],
-                'lname' => $row['last_name'],
-                'phone' => $row['telephone'],
-                'birthdate' => $row['birthdate'],
-                'city' => $row['city'],
-                'address' => $row['address'],
-                'postal' => $row['postal_code'],
-                'location' => $row['first_name'],
-                'count' => $row['first_name'],
-                'apptime' => $row['first_name'],
-                'appbirthdate' => $row['first_name']
-            ]);
-        }
+        $this->latitudee($row[5]);
+        $this->camp = campaigns::where('name',$row[10])->get();
+        $this->admin = Admins::where('email',$row[11])->get();
+
+
+        return new lead([
+            'first_name' => $row[0],
+            'last_name' => $row[1],
+            'telephone' => $row[2],
+            'birthdate' => $row[3],
+            'city' => $row[4],
+            'address' => $row[5],
+            'postal_code' => $row[6],
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+            'number_of_persons' => $row[7],
+            'nationality' => $row[8],
+            'appointment_date' => $row[9],
+            'campaign_id' => $this->camp[0]->id,
+            'assign_to_id' => $this->admin[0]->id,
+            'time' => $row[12],
+
+        ]);
     }
 }
--- resources/views/insterappointment.blade.php
+++ resources/views/insterappointment.blade.php
@@ -78,13 +78,24 @@
                         </div>
 
                         <input type="submit" class="btn btn-primary" value="Create">
-                        <br>
+                        <br><br>
 
 
                     </div>
                 </div>
             </form>
             Or insert by file
+            <div  onclick="openExamplePic()">
+                Example:
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-down" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                    <path fill-rule="evenodd" d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                </svg>
+            </div><br>
+            <div style="display: none"   class="w-100" id="picture">
+                <img src="img/exceExample.png" alt="pic" class="img-fluid">
+                <hr>
+            </div>
             <form action="{{route('addappointmentfile')}}" method="post" enctype="multipart/form-data">
                 @csrf
                 <div class="input-group">
@@ -92,9 +103,21 @@
                 <input type="submit" value="Submit" class="btn btn-info">
                 </div>
             </form>
+
+
         </div>
     </div>
     <script type="text/javascript">
+        function openExamplePic(){
+            var x = document.getElementById('picture');
+            if (x.style.display == 'none'){
+                x.style.display = "block";
+            }
+            else{
+                x.style.display = "none";
+            }
+        }
+
         function hideadmin() {
             var x = document.getElementById('selecti').value;
             if (x == "no") {
